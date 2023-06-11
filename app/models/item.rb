a332aec99f129be887cdfe719469b22b2f1cc465
AVLESSI Matchoudi <matchoudiavlessi@gmail.com>
2023-06-11 13:12:02 +0000

    Add notification relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
     belongs_to :user, class_name: 'User', foreign_key: 'user_id'
     belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
+    has_one :notification
 
     enum :status, { no_viewed: 0, viewed: 1 }
 
