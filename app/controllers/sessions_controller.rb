d23fe0f385d92d73c2180e37aa3acb8f6ff44610
AVLESSI Matchoudi <matchoudiavlessi@gmail.com>
2023-06-15 16:44:56 +0000

    Implement google sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,9 +17,30 @@ class SessionsController < ApplicationController
     def google_oauth
         client_id = Rails.application.credentials.google_client_id
         begin
-            data = Google::Auth::IDTokkens.verify_oidc params[:token], aud: client_id
+            data = Google::Auth::IDTokens.verify_oidc params[:token], aud: client_id
+            user_check = User.find_by_email(data['email'])
+            if(user_check)
+                user_check.update(avatar_url: data['picture']) if user_check.avatar_url.nil?
+            else
+                user_check = create_new_user(data)
+            end
+
+            token = jwt_encode(user_id: user_check.id)
+            render json: { user: UserSerializer.new(user_check), token: token }, status: :ok
         rescue StandardError => e
             render json: { error: e.message }, status: :unauthorized
         end
+    end
+
+    private
+
+    def create_new_user (data)
+        password = SucureRandom.base64(15)
+        new_user = User.create(
+            fullname: data['name'],
+            email: data['email'],
+            avatar_url: data['picture']
+            password: password
+        )
     end
 end
