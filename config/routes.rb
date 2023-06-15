593a8dc4ca7f818378b44bd5819bd92f58e1f908
AVLESSI Matchoudi <matchoudiavlessi@gmail.com>
2023-06-15 01:33:59 +0000

    Set up websocket server

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # action cable server
+  mount ActionCable.server => '/cable'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
